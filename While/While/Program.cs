2cfa6ed995e18abb004b0e8f37e5943e779a6294
Daniel-Hercules <73712933+Daniel-Hercules@users.noreply.github.com>
2020-11-02 17:23:59 +0000

    Add files via upload

---
+++ While/While/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Counter using while loop
+            int counter = 0;
+            int limit = 10; 
+            while(counter <= limit)
+            {
+                Console.WriteLine("Counter is at: " + counter);
+                counter++;
+            }
+        }
+    }
+}
